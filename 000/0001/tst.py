e28e8d7586f3dcc7f38ce64369eae2ee142f38b0
Ewa <bittner.ewa@gmail.com>
2020-02-26 21:54:30 +0100

    Improved version.

--- 000/0001/tst.py
+++ 000/0001/tst.py
@@ -5,32 +5,19 @@
 
 def main():
 
-    my_list3 = []
-    my_list5 = []
+    my_list = list()
 
     number1 = 3
     number2 = 5
 
     for a in range(1000):
-        result = a * number1
-        my_list3.append(result)
-        a += 1
+        if a % number1 == 0 or a % number2 == 0:
+              my_list.append(a)
 
-    multiplication_sum_3 = sum(my_list3)
-
-    print("The sum of all multiples of 3 below 1000 is ", multiplication_sum_3)
-    print(my_list3)
+    multiplication_sum = sum(my_list)
+    print("The list of number below 1000 that can be divided by 3 or 5: ", my_list)
     print()
-
-    for b in range(1000):
-        result = b * number2
-        my_list5.append(result)
-        b += 1
-
-    multiplication_sum_5 = sum(my_list5)
-
-    print("The sum of all multiples of 5 below 1000 is ", multiplication_sum_5)
-    print(my_list5)
+    print("The sum of all these numbers is ", multiplication_sum)
 
 
 main()
